1dd54c59a61a25cac1c56dd7296a22134e3cbf03
pepe <pepe@DESKTOP-FVE60BB>
2020-11-06 11:05:59 +0100

    Add project files.

---
+++ Models/ImageModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageUpload.Models
+{
+    public class ImageModel
+    {
+        [Key]
+        public int ImageId { get; set; }
+
+        [Column(TypeName ="nvarchar(50)") ]
+        public string Title { get; set; }
+        
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Image Name")]
+        public string ImageName { get; set; }
+
+        [NotMapped]
+        [DisplayName("Upload file")]
+        public IFormFile ImageFile { get; set; }
+    }
+}
